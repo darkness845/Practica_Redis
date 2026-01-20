921ad14a49063af73b7a6cebd6ee5885cf23a90e
anouartahir <trabajoanouar@gmail.com>
2026-01-20 18:44:01 +0100

    arreglos en el funcionamiento y importar dotenv en supabase.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
 
+import equiposRoutes from './routes/equipos.routes.js';
+import jugadoresRoutes from './routes/jugadores.routes.js';
+
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("<h1>Hola imbecil</h1>")
-})
+app.use('/api/equipos', equiposRoutes);
+app.use('/api/jugadores', jugadoresRoutes);
+
+app.get('/', (req, res) => {
+  res.send('<h1>API funcionando</h1>');
+});
 
 export default app;
